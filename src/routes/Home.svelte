a55700050d75e875a5d0f66c46aa73c0340792e1
yajatvishwak <yajat472001@gmail.com>
2022-03-02 13:54:52 +0530

    new ui

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -18,7 +18,13 @@
       {
         bucketID: "Thisisbro",
         bucketName: "Super Awesome Bucket of yajat vishwakarma",
-        imageList: ["asset/1.png", "asset/2.png", "asset/3.png"],
+        imageList: [
+          "asset/1.png",
+          "asset/2.png",
+          "asset/3.png",
+          "asset/3.png",
+          "asset/3.png",
+        ],
         votesOnBucket: 23,
       },
     ],
@@ -49,48 +55,19 @@
   </div>
 </div>
 
-<section class="h-full w-full flex  flex-col p-5 pt-2.5 lg:p-10 lg:pt-5">
+<section class="h-full w-full flex  flex-col p-5 pt-2.5 lg:p-10 lg:pt-5 mx- ">
   <NavBar />
-  <div class="text-4xl mt-10">
-    {greetingTime(new Date())}
-    <span class="font-bold">Danny Boi</span>
-    {emojis[parseInt(Math.random() * emojis.length)]}
-  </div>
-  <div class="mt-10 flex-col flex lg:flex-row justify-between">
-    <div id="stats" class="flex flex-wrap justify-center lg:justify-start ">
-      <div class="p-5 lg:m-5 bg-yellow-2000 rounded-2xl">
-        <div class="newfont text-8xl font-bold text-yellow-500">
-          {data.noOfBuckets}
-        </div>
-        <div class="newfont text-center lg:text-left"># of buckets</div>
-      </div>
-      <div class="p-5 lg:m-5 bg-yello-200 rounded-2xl">
-        <div class="newfont text-8xl font-bold text-yellow-500">
-          {data.noOfVotes}
-        </div>
-        <div class="newfont text-center lg:text-left"># of likes</div>
-      </div>
-      <div class="p-5 lg:m-5 bg-yello-200 rounded-2xl">
-        <div class="newfont text-8xl font-bold text-yellow-500">
-          {data.maxVotes}
-        </div>
-        <div class="newfont text-center lg:text-left">Most likes</div>
-      </div>
+  <div class="mx-auto  w-full max-w-screen-xl">
+    <div class="text-4xl mt-10">
+      {greetingTime(new Date())}
+      <span class="font-bold ">Danny Boi</span>
+      {emojis[parseInt(Math.random() * emojis.length)]}
     </div>
-
-    <div class="p-5 bg-yellow-200 rounded-2xl  lg:w-1/3 text-black">
-      <div class="text-2xl mb-3 font-bold">Hmmm... what's pickforest bro?</div>
-      <div class="opacity-50 mb-5 newfont">
-        Have you been in a situation where you are tryna post something on your
-        social account but you are in distress as to which of the 178 pictures
-        to post? (Trust me, been there)
-      </div>
-      <label for="my-modal3" class="btn modal-button">Take a Tour</label>
+    <div class="mt-10 mb-4 text-2xl max-w-screen-lgfont-bold ">
+      Your Buckets
     </div>
   </div>
-
-  <div class="mt-10 mb-4 text-2xl max-w-screen-lgfont-bold ">Your Buckets</div>
-  <div class="grid lg:grid-cols-3  gap-4 ">
+  <div class="grid lg:grid-cols-2 max-w-screen-xl mx-auto  gap-10 ">
     {#each data.bucketDetails as bucket}
       <DashboardCard
         bucketID={bucket.bucketID}
@@ -122,8 +99,9 @@
 
       <div>Add Bucket</div>
     </label>
-    <div
-      class="w-full hover:scale-95 transition-all rounded-box font-bold dark:bg-slate-800 bg-gray-100 h-56 flex flex-col justify-center items-center"
+    <label
+      for="my-modal3"
+      class="w-full hover:scale-95 modal-button transition-all rounded-box font-bold dark:bg-slate-800 bg-gray-100 h-56 flex flex-col justify-center items-center"
     >
       <div class="dark:block hidden">
         <lord-icon
@@ -142,7 +120,8 @@
         />
       </div>
       <div>New here?</div>
-    </div>
+    </label>
+
     {#if data.bucketDetails.length < 1}
       <div
         class="w-full hover:scale-95 transition-all rounded-box font-bold bg-gray-100 h-56 flex flex-col justify-center items-center"
